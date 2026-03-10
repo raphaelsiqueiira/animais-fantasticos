282fc3b26a034dffb9719aadf88d0745c2bab421
Raphael Borba <raphaelborba10@gmail.com>
2026-03-10 07:22:14 -0300

    Primeiro commit

---
+++ js/modules/fetch-animais.js
@@ -0,0 +1,27 @@
+import initAnimaNumeros from "./anima-numeros.js";
+export default function initFetchAnimais() {
+  async function fetchAnimais(url) {
+    try {
+      const animaisJSON = await (await fetch(url)).json();
+      const numerosGrid = document.querySelector(".numeros-grid");
+
+      animaisJSON.forEach((animal) => {
+        const divAnimal = createAnimal(animal);
+        numerosGrid.appendChild(divAnimal);
+      });
+      initAnimaNumeros();
+    } catch (erro) {
+      console.log(erro);
+    }
+  }
+
+  function createAnimal(animal) {
+    const div = document.createElement("div");
+    div.classList.add("numero-animal");
+
+    div.innerHTML = `<h2>${animal.especie}</h2><span data-numero>${animal.total}</span>`;
+    return div;
+  }
+
+  fetchAnimais("./animais-api.json");
+}
---
+++ js/script.js
@@ -0,0 +1,24 @@
+import initAccordion from "./modules/accordion.js";
+
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
+import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initFuncionamento from "./modules/funcionamento.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initModal from "./modules/modal.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import initTabNav from "./modules/tabnav.js";
+import initTooltip from "./modules/tooltip.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();
