fb33698627d38ac60fa75f862d36f2db3f267242
Raphael Borba <raphaelborba10@gmail.com>
2026-03-10 19:25:00 -0300

    tabNav transformado em class

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initModal from "./modules/modal.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import ScrollSuave from "./modules/scroll-suave.js";
-import initTabNav from "./modules/tabnav.js";
+import TabNav from "./modules/tabnav.js";
 import initTooltip from "./modules/tooltip.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
@@ -16,8 +16,13 @@ scrollSuave.init();
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
+const tabNav = new TabNav(
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section'
+);
+tabNav.init();
+
 initAnimacaoScroll();
-initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
